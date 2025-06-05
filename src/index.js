7d0da23504fb9ad690449763b681434f3723e3a3
mcgj8847 <mcguzmanj@uoc.edu>
2025-06-05 11:50:34 -0600

    Updated Html and Js Files

--- src/index.js
+++ src/index.js
@@ -17,6 +17,33 @@ function displayWeather(response) {
   temperatureElement.innerHTML = temperature;
   let cityElement = document.querySelector("#current-city");
   cityElement.innerHTML = response.data.city;
+  let descriptionElement = document.querySelector("#weather-descripcion");
+  descriptionElement.innerHTML = response.data.condition.description;
 }
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+let currentDateELement = document.querySelector("#current-date");
+let currentDate = new Date();
+currentDateELement.innerHTML = formatDate(currentDate);
 let form = document.querySelector("form");
 form.addEventListener("submit", search);
